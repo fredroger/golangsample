e78d13f9cbe05c84caa065c19ae68ff39a3a320c
fred_roger <fredroger@gmail.com>
2017-01-15 19:14:37 -0500

    refactoring

--- main.go
+++ main.go
@@ -1,21 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"html"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
-	log.Fatal(http.ListenAndServe(":8080", router))
-}
+	router := NewRouter()
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
